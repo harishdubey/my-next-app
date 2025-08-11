67939d302e1a8fae3d5c1f4b54b98334ad0ede25
harishdubey <duha.1234@gmail.com>
2025-08-11 18:21:50 +0530

    changes done

---
+++ src/app/about/[...not-found]/page.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h1>About us Page not found</h1>;
+}
--- src/app/study/[...lecture]/page.tsx
+++ src/app/study/[...lecture]/page.tsx
@@ -1,3 +1,11 @@
-export default function Lecture() {
-  return <h1>Lecture</h1>;
+"use client";
+export default function Lecture({ params }) {
+  console.log(params, "params lec");
+  return (
+    <div>
+      <h1>Lecture</h1>
+      <div> Study Page : {params.lecture[0]}</div>
+      <div>Lecture : {params.lecture[1]}</div>
+    </div>
+  );
 }
